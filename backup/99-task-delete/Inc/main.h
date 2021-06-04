e389d85a69f3ca5fea70df4768c5d9d23f124e68
bv <baovu79@protonmail.com>
2021-06-04 17:52:15 +0300

    upload

---
+++ backup/99-task-delete/Inc/main.h
@@ -0,0 +1,20 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "stm32l1xx_hal.h"
+#include "os.h"
+#include "bsp_clk.h"
+#include "bsp_led.h"
+#include "bsp_usart.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
