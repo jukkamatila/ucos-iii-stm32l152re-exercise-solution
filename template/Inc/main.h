807ce6a0fd9fdc9ac06b6b60ed4c11b855304bed
bv <baovu79@protonmail.com>
2021-06-04 17:54:33 +0300

    update

---
+++ template/Inc/main.h
@@ -0,0 +1,17 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include "stm32l1xx_hal.h"
+#include "os.h"
+#include "bsp_clk.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
