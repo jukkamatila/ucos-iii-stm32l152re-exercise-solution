56bd222a684724ab2a4c640952ba6553966ac14c
bv <baovu79@protonmail.com>
2021-06-05 15:22:41 +0300

    update

--- 04-priority-inversions/Src/main.c
+++ 04-priority-inversions/Src/main.c
@@ -17,43 +17,40 @@ static CPU_STK AppTaskStartStk[APP_TASK_START_STK_SIZE];
 static void AppTaskStart(void *p_arg);
 
 /*******************************************************************************
- * Task 1: Defines and Variables
- * Task 1 reads data from BMP280
- * (High priority)
+ * Read Task: Defines and Variables
+ * reads data from BMP280
  ******************************************************************************/ 
-#define TASK_1_STK_SIZE 256u
-#define TASK_1_PRIO 2u
-static OS_TCB Task1TCB;
-static CPU_STK Task1Stk[TASK_1_STK_SIZE];
-static void Task1(void *p_arg);
+#define READTASK_STK_SIZE 256u
+#define READTASK_PRIO 4u
+static OS_TCB ReadTaskTCB;
+static CPU_STK ReadTaskStk[READTASK_STK_SIZE];
+static void ReadTask(void *p_arg);
 
 /*******************************************************************************
  * Task 2: Defines and Variables
  * Task 2 send data to UART (temperature and humidity)
- * (Medium priority)
  ******************************************************************************/ 
 #define TASK_2_STK_SIZE 256u
-#define TASK_2_PRIO 3u
-static OS_TCB Task2TCB;
-static CPU_STK Task2Stk[TASK_2_STK_SIZE];
-static void Task2(void *p_arg);
+#define TASK_2_PRIO 2u
+static OS_TCB PrintTaskTCB;
+static CPU_STK PrintTaskStk[TASK_2_STK_SIZE];
+static void PrintTask(void *p_arg);
 
 /*******************************************************************************
- * Task 3: Defines and Variables
- * Task 3 blinks LED 3 times
- * (Low priority)
+ * Blink Task: Defines and Variables
+ * Blink Task blinks LED 3 times
  ******************************************************************************/ 
-#define TASK_3_STK_SIZE 1024u
-#define TASK_3_PRIO 4u
-static OS_TCB Task3TCB;
-static CPU_STK Task3Stk[TASK_3_STK_SIZE];
-static void Task3(void *p_arg);
+#define BLINK_TASK_STK_SIZE 1024u
+#define BLINK_TASK_PRIO 3u
+static OS_TCB BlinkTaskTCB;
+static CPU_STK BlinkTaskStk[BLINK_TASK_STK_SIZE];
+static void BlinkTask(void *p_arg);
+void Blink();
 
 /*******************************************************************************
  * Shared resources
  ******************************************************************************/ 
 OS_SEM task_sync_sem;
-uint8_t is_bmp280_read = 0;
 
 /*******************************************************************************
  * Main function
@@ -71,7 +68,7 @@ int main(void)
   OSSemCreate(
       (OS_SEM *)&task_sync_sem,
       (CPU_CHAR *)"Task Synchronize Semaphore",
-      (OS_SEM_CTR)1,
+      (OS_SEM_CTR)0,
       (OS_ERR *)&os_err);
 
   OSTaskCreate(
@@ -111,14 +108,14 @@ static void AppTaskStart(void *p_arg)
     BMP280_Setup();
 
     OSTaskCreate(
-        (OS_TCB *)&Task1TCB,
-        (CPU_CHAR *)"Task 1",
-        (OS_TASK_PTR)Task1,
+        (OS_TCB *)&ReadTaskTCB,
+        (CPU_CHAR *)"Read Task",
+        (OS_TASK_PTR)ReadTask,
         (void *)0,
-        (OS_PRIO)TASK_1_PRIO,
-        (CPU_STK *)&Task1Stk[0],
-        (CPU_STK_SIZE)TASK_1_STK_SIZE / 10,
-        (CPU_STK_SIZE)TASK_1_STK_SIZE,
+        (OS_PRIO)READTASK_PRIO,
+        (CPU_STK *)&ReadTaskStk[0],
+        (CPU_STK_SIZE)READTASK_STK_SIZE / 10,
+        (CPU_STK_SIZE)READTASK_STK_SIZE,
         (OS_MSG_QTY)5u,
         (OS_TICK)0u,
         (void *)0,
@@ -126,12 +123,12 @@ static void AppTaskStart(void *p_arg)
         (OS_ERR *)&os_err);
 
     OSTaskCreate(
-        (OS_TCB *)&Task2TCB,
+        (OS_TCB *)&PrintTaskTCB,
         (CPU_CHAR *)"Task 2",
-        (OS_TASK_PTR)Task2,
+        (OS_TASK_PTR)PrintTask,
         (void *)0,
         (OS_PRIO)TASK_2_PRIO,
-        (CPU_STK *)&Task2Stk[0],
+        (CPU_STK *)&PrintTaskStk[0],
         (CPU_STK_SIZE)TASK_2_STK_SIZE / 10,
         (CPU_STK_SIZE)TASK_2_STK_SIZE,
         (OS_MSG_QTY)5u,
@@ -141,14 +138,14 @@ static void AppTaskStart(void *p_arg)
         (OS_ERR *)&os_err);
 
     OSTaskCreate(
-        (OS_TCB *)&Task3TCB,
-        (CPU_CHAR *)"Task 3",
-        (OS_TASK_PTR)Task3,
+        (OS_TCB *)&BlinkTaskTCB,
+        (CPU_CHAR *)"Blink Task",
+        (OS_TASK_PTR)BlinkTask,
         (void *)0,
-        (OS_PRIO)TASK_3_PRIO,
-        (CPU_STK *)&Task3Stk[0],
-        (CPU_STK_SIZE)TASK_3_STK_SIZE / 10,
-        (CPU_STK_SIZE)TASK_3_STK_SIZE,
+        (OS_PRIO)BLINK_TASK_PRIO,
+        (CPU_STK *)&BlinkTaskStk[0],
+        (CPU_STK_SIZE)BLINK_TASK_STK_SIZE / 10,
+        (CPU_STK_SIZE)BLINK_TASK_STK_SIZE,
         (OS_MSG_QTY)5u,
         (OS_TICK)0u,
         (void *)0,
@@ -156,13 +153,38 @@ static void AppTaskStart(void *p_arg)
         (OS_ERR *)&os_err);
 }
 
-static void Task1(void *p_arg)
+static void ReadTask(void *p_arg)
 {
     OS_ERR os_err;
     
     while (DEF_TRUE)
     {
-        HAL_UART_Transmit(&huart2, (uint8_t *)"\rTask 1 - OSSemPend\n\r", 22, 100);
+        uint8_t msg[] = "\rRead Task - run\n\r";
+        HAL_UART_Transmit(&huart2, msg, sizeof(msg), 100);
+
+        Delay_Blocking(5000);
+        BMP280_Read();
+
+        HAL_UART_Transmit(&huart2, (uint8_t *)"\rRead Task - OSSemPost\n\r", 25, 100);
+        OSSemPost(
+            (OS_SEM *)&task_sync_sem,
+            (OS_OPT)OS_OPT_POST_1,
+            (OS_ERR *)&os_err);
+        HAL_UART_Transmit(&huart2, (uint8_t *)"\rRead Task - end\n\r", 19, 100);
+
+        OSTimeDlyHMSM(0, 0, 6, 0, OS_OPT_TIME_HMSM_STRICT, &os_err);
+    }
+}
+
+static void PrintTask(void *p_arg)
+{
+    OS_ERR os_err;
+
+    while (DEF_TRUE)
+    {
+        uint8_t msg[] = "\rPrint Task - run\n\r";
+        HAL_UART_Transmit(&huart2, msg, sizeof(msg), 100);
+        HAL_UART_Transmit(&huart2, (uint8_t *)"\rPrint Task - OSSemPend\n\r", 26, 100);
         OSSemPend(
             (OS_SEM *)&task_sync_sem,
             (OS_TICK)0,
@@ -170,72 +192,42 @@ static void Task1(void *p_arg)
             (CPU_TS *)NULL,
             (OS_ERR *)&os_err);
 
-        uint8_t msg[] = "\rTask 1 run\n\r";
-        HAL_UART_Transmit(&huart2, msg, sizeof(msg), 100);
-        if(is_bmp280_read == 0)
-        {
-            BMP280_Read();
-            is_bmp280_read = 1;
-        }
-        HAL_UART_Transmit(&huart2, (uint8_t *)"\rTask 1 - OSSemPost\n\r", 22, 100);
-        OSSemPost(
-            (OS_SEM *)&task_sync_sem,
-            (OS_OPT)OS_OPT_POST_1,
-            (OS_ERR *)&os_err);
-
-        OSTimeDlyHMSM(0, 0, 1, 0, OS_OPT_TIME_HMSM_STRICT, &os_err);
-    }
-}
-
-static void Task2(void *p_arg)
-{
-    OS_ERR os_err;
-
-    while (DEF_TRUE)
-    {
-        uint8_t msg[] = "\rTask 2 run\n\r";
-        HAL_UART_Transmit(&huart2, msg, sizeof(msg), 100);
+        HAL_UART_Transmit(&huart2, (uint8_t *)"\rPrint Task - resume\n\r", 23, 100);
         BMP280_Print(&huart2);
-        OSTimeDlyHMSM(0, 0, 2, 0, OS_OPT_TIME_HMSM_STRICT, &os_err);
+
+        HAL_UART_Transmit(&huart2, (uint8_t *)"\rPrint Task - end\n\r", 20, 100);
+
+        OSTimeDlyHMSM(0, 0, 6, 0, OS_OPT_TIME_HMSM_STRICT, &os_err);
     }
 }
 
-static void Task3(void *p_arg)
+static void BlinkTask(void *p_arg)
 {
     OS_ERR os_err;
 
     while (DEF_TRUE)
     {
-        HAL_UART_Transmit(&huart2, (uint8_t *)"\rTask 3 - OSSemPend\n\r", 22, 100);
-        OSSemPend(
-            (OS_SEM *)&task_sync_sem,
-            (OS_TICK)0,
-            (OS_OPT)OS_OPT_PEND_BLOCKING,
-            (CPU_TS *)NULL,
-            (OS_ERR *)&os_err);
 
-        uint8_t msg[] = "\rTask 3 run\n\r";
+        uint8_t msg[] = "\rBlink Task - run\n\r";
         HAL_UART_Transmit(&huart2, msg, sizeof(msg), 100);
 
-        if(is_bmp280_read == 1)
+        for(int i = 0; i < 3; i++)
         {
-            for(int i = 0; i < 3; i++)
-            {
-                HAL_UART_Transmit(&huart2, (uint8_t*)"\rBlink\n\r", 9, 100);
-                LED_On(GPIOA, GPIO_PIN_5);
-                Delay_Blocking(500);
-                LED_Off(GPIOA, GPIO_PIN_5);
-                Delay_Blocking(500);
-            }
-            is_bmp280_read = 0;
+            uint8_t countmsg[12];
+            sprintf((char*)countmsg, "\rBlink %d\n\r", i+1);
+            HAL_UART_Transmit(&huart2,countmsg, sizeof(countmsg) , 100);
+            Blink();
         }
+        HAL_UART_Transmit(&huart2, (uint8_t *)"\rBlink Task - end\n\r", 20, 100);
 
-        HAL_UART_Transmit(&huart2, (uint8_t *)"\rTask 3 - OSSemPost\n\r", 22, 100);
-        OSSemPost(
-            (OS_SEM *)&task_sync_sem,
-            (OS_OPT)OS_OPT_POST_1,
-            (OS_ERR *)&os_err);
-
-        OSTimeDlyHMSM(0, 0, 0, 500, OS_OPT_TIME_HMSM_STRICT, &os_err);
+        OSTimeDlyHMSM(0, 0, 0, 3, OS_OPT_TIME_HMSM_STRICT, &os_err);
     }
+}
+
+void Blink()
+{
+    LED_On(GPIOA, GPIO_PIN_5);
+    Delay_Blocking(500);
+    LED_Off(GPIOA, GPIO_PIN_5);
+    Delay_Blocking(500);
 }
